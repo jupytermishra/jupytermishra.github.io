59ed079cde73897a325529a95fc8a1312c576070
jupytermishra <165263595+jupytermishra@users.noreply.github.com>
2024-04-13 22:04:14 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// JavaScript code for social media sharing functionality
-document.querySelectorAll('.share-button').forEach(button => {
-    button.addEventListener('click', () => {
-        // Implement sharing functionality here
-        alert('Share button clicked!');
-    });
-});
