fb2c90eb38c56ec34959798e3a4ef80a43f28101
jupytermishra <165263595+jupytermishra@users.noreply.github.com>
2024-04-13 21:53:28 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// JavaScript code for social media sharing functionality
+document.querySelectorAll('.share-button').forEach(button => {
+    button.addEventListener('click', () => {
+        // Implement sharing functionality here
+        alert('Share button clicked!');
+    });
+});
